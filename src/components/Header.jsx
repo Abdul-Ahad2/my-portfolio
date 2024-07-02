a5c046b3d5767cc70a97b3be5137f5e1ae064c7d
Abdul Ahad <abdulahadhaq@outlook.com>
2024-07-02 23:02:41 +0500

    about page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,8 @@ export default function Header() {
   return (
     <>
       <div className="bg-black w-full h-[12vh] flex text-white items-center justify-between px-4 font-sans">
-        <div className="text-4xl font-thin  font-dmsans ">Usman.</div>
-        <div className="font-thin">
+        <div className="text-4xl font-thin font-dmsans">Usman.</div>
+        <div className="font-thin flex items-center">
           <Link className="m-2" to="/">
             Home
           </Link>
@@ -22,7 +22,13 @@ export default function Header() {
           <Link className="m-2" to="/contact">
             Contact
           </Link>
-          <Link className="bg-white rounded-full w-1/2 h-3">pic</Link>
+          <Link className="w-10 h-10 bg-white rounded-full overflow-hidden mx-3">
+            <img
+              src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=800"
+              alt="Profile"
+              className="w-full h-full object-cover"
+            />
+          </Link>
         </div>
       </div>
     </>
